9232cb3f730874eb2e8e902a354725adb8d1bf58
Sanjaya1717 <imaderikiwidiastanasanjaya@gmail.com>
2024-01-10 17:33:29 +0800

    fix export schema

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -6,14 +6,24 @@ class PlaylistsService {
   }
 
   getPlaylist = async (playlistId) => {
-    const query = {
-      text: `SELECT playlists.*, users.username FROM playlists
-      LEFT JOIN users ON users.id = playlists.owner
+    const songQuery = {
+      text: `SELECT songs.id, songs.title, songs.performer FROM playlist_songs
+      LEFT JOIN songs ON songs.id = playlist_songs.song_id
+      WHERE playlist_songs.playlist_id = $1`,
+      values: [playlistId],
+    };
+    const songs = await this._pool.query(songQuery);
+
+    const playlistQuery = {
+      text: `SELECT playlists.id, playlists.name FROM playlists
       WHERE playlists.id = $1`,
       values: [playlistId],
     };
-    const result = await this._pool.query(query);
-    return result.rows[0];
+    const playlistResult = await this._pool.query(playlistQuery);
+    return {
+      ...playlistResult.rows[0],
+      songs: songs.rows,
+    };
   };
 }
 
--- src/listener.js
+++ src/listener.js
@@ -8,7 +8,7 @@ class Listener {
     try {
       const { playlistId, targetEmail } = JSON.parse(message.content.toString());
       const playlist = await this._playlistsService.getPlaylist(playlistId);
-      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlist));
+      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify({ playlist }));
       console.log(result);
     } catch (error) {
       console.error(error);
